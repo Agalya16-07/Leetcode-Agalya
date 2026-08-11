baa13b4252af589991dfdc6ad5d65c3be045ad19
Agalya16-07 <agalyaramesh1625@gmail.com>
2026-08-11 17:51:49 +0530

    Create 841-ShortestDistanceToACharacter

---
+++ 841-ShortestDistanceToACharacter/841-ShortestDistanceToACharacter.java
@@ -0,0 +1,30 @@
+// Last updated: 8/11/2026, 5:51:49 PM
+class Solution {
+    public int[] shortestToChar(String s, char c) {
+
+        int n = s.length();
+        int[] ans = new int[n];
+
+        // Left to Right
+        int prev = -n;
+
+        for (int i = 0; i < n; i++) {
+            if (s.charAt(i) == c) {
+                prev = i;
+            }
+            ans[i] = i - prev;
+        }
+
+        // Right to Left
+        prev = 2 * n;
+
+        for (int i = n - 1; i >= 0; i--) {
+            if (s.charAt(i) == c) {
+                prev = i;
+            }
+            ans[i] = Math.min(ans[i], prev - i);
+        }
+
+        return ans;
+    }
+}
