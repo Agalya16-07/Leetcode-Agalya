7966e7df9af89f90abba08a2002c4c5724a1a18d
Agalya16-07 <agalyaramesh1625@gmail.com>
2026-08-11 17:47:45 +0530

    Create 3275-MinimumNumberOfPushesToTypeWordI

---
+++ 3275-MinimumNumberOfPushesToTypeWordI/3275-MinimumNumberOfPushesToTypeWordI.java
@@ -0,0 +1,12 @@
+// Last updated: 8/11/2026, 5:47:45 PM
+class Solution {
+    public int minimumPushes(String word) {
+        int ans = 0;
+
+        for (int i = 0; i < word.length(); i++) {
+            ans += (i / 8) + 1;
+        }
+
+        return ans;
+    }
+}
