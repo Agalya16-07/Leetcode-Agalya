ded8378721660f944b6d5f9b7306272f0cfe524a
Agalya16-07 <agalyaramesh1625@gmail.com>
2026-08-11 17:47:21 +0530

    Create 4355-RearrangeStringToAvoidCharacterPair

---
+++ 4355-RearrangeStringToAvoidCharacterPair/4355-RearrangeStringToAvoidCharacterPair.java
@@ -0,0 +1,23 @@
+// Last updated: 8/11/2026, 5:47:21 PM
+class Solution {
+    public String rearrangeString(String s, char x, char y) {
+        int[] freq = new int[26];
+        for(char c: s.toCharArray())
+            freq[c-'a']++;
+        
+        StringBuilder ans = new StringBuilder();
+        for(char c='a'; c<='z'; c++){
+            if(c==x || c==y)
+                continue;
+            while(freq[c-'a']-->0)
+                ans.append(c);
+        }
+        while(freq[y-'a']-->0){
+            ans.append(y);
+        }
+        while(freq[x-'a']-->0)
+            ans.append(x);
+        return ans.toString();
+        
+    }
+}
